d3a37152d8b4cdfc7be39fc04bc78dbf01064600
Moe Ayoub <mma.ayoub@outlook.com>
2024-03-19 10:06:37 -0500

    Initial Next.js w/ TypeScript & TailwindCSS commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import { GeistSans } from "geist/font/sans"
+import "./globals.css"
+
+const defaultUrl = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000"
+
+export const metadata = {
+    metadataBase: new URL(defaultUrl),
+    title: "Moe Ayoub - Portfolio",
+    description: "The fastest way to build apps with Next.js",
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en" className={GeistSans.className}>
+            <body className="bg-background text-foreground">
+                <main>
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
+}
